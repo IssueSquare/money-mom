1c80203b7b66062ec846e659dfb2d7e83eac48eb
4406arthur <arthurma@loftechs.com>
2018-02-04 21:27:40 +0800

    user service secure with Basic Token now!

--- main.go
+++ main.go
@@ -6,10 +6,25 @@ import (
 	"net/http"
 
 	"github.com/IssueSquare/money-mom/cmd/user"
+	"github.com/IssueSquare/money-mom/pkg/auth"
 )
 
+type basicCredential struct {
+	clientID string
+	password string
+}
+
 func main() {
-	http.HandleFunc("/userRegister", user.Register)
-	http.HandleFunc("/userLogin", user.Login)
+
+	//TODO load Basic account from DB
+	bc := &basicCredential{
+		clientID: "ios-x02",
+		password: "testpass",
+	}
+
+	//User api group should secure with Basic token.
+	http.HandleFunc("/userRegister", auth.BasicAuth(user.Register, []byte(bc.clientID), []byte(bc.password)))
+	http.HandleFunc("/userLogin", auth.BasicAuth(user.Login, []byte(bc.clientID), []byte(bc.password)))
+
 	http.ListenAndServe(":8080", nil)
 }
